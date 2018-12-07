4e69b4274cf7cb9ee45df53e2f7a8232e7d1b4b7
FWao <fabian.wolf2@mailbox.tu-dresden.de>
2018-12-07 14:53:43 +0100

    hard work digit sum

--- app/src/main/java/org/emi/uebung/emiapp/MainActivity.java
+++ app/src/main/java/org/emi/uebung/emiapp/MainActivity.java
@@ -2,6 +2,9 @@ package org.emi.uebung.emiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String inputNumber = Integer.toString(1234);
+                int[] digits = new int[inputNumber.length()];
+                int digitSum = 0;
+                for (int i = 0; i < digits.length; i++) {
+                    digits[i] = inputNumber.charAt(i) - '0';
+                    digitSum += digits[i];
+                }
+
+                Log.d("Output: ", Integer.toString(digitSum) );
+                Log.d("count: ", Integer.toString(digits.length));
+
+            }
+        });
+
+
     }
 }
